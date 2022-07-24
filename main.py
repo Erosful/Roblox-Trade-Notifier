2928ab6b2b54c57ad8b5de89cbdddf18977ae65d
Erosful <101930449+Erosful@users.noreply.github.com>
2022-07-24 16:11:36 +0100

    Robux + Win/Lose Separators
    Added Robux to the Trade notifier, + made a separate webhook for wins and losses

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ except: #and if you don't, it'll install it for you!
 with open("config.json","r") as f:
     data = json.load(f)
 cookie = data['Cookie']
-url = data['WebhookURL']
+WinWebhook = data['WinWebhook']
+LoseWebhook = data['LoseWebhook']
 DiscordID = data['Discord ID']
 
 def sendnotif(tradejson):
@@ -20,6 +21,7 @@ def sendnotif(tradejson):
     for a,v in enumerate(tradejson['offers']): #This bit iterates through the offers, and calculates the final values
         for i in v['userAssets']:
             TradeValues[a] += rolivalues[str(i['assetId'])]['Value']
+        TradeValues[a] += v['robux']
     #With this, be careful what you edit (I advise you look at https://leovoel.github.io/embed-visualizer/ for more info)
     data = {
         "username" : "Trade Notifier", #You don't NECESSARILY need to have this and the avatar_url field, I just felt like including it [You could change the name on the webhook every time]
@@ -33,11 +35,11 @@ def sendnotif(tradejson):
                 "fields": [
                     {
                         "name": "Requesting", #This might be a bit confusing, sorry! Essentially, I add a :warning: if the item is projected, then I post the item name and a link to that item - "\n".join() is just an easier way of sending them all in separate lines
-                        "value": "\n".join(f"{':warning: ' if rolivalues[str(i['assetId'])]['Projected'] else ''} [{i['name']}](https://www.roblox.com/catalog/{i['assetId']}) ({rolivalues[str(i['assetId'])]['Value']:,d})" for i in tradejson['offers'][0]['userAssets'])+f"\n**Total Value:** {TradeValues[0]:,d}"
+                        "value": "\n".join(f"{':warning: ' if rolivalues[str(i['assetId'])]['Projected'] else ''} [{i['name']}](https://www.roblox.com/catalog/{i['assetId']}) ({rolivalues[str(i['assetId'])]['Value']:,d})" for i in tradejson['offers'][0]['userAssets'])+f"\n**Robux**: {tradejson['offers'][0]['robux']}\n**Total Value:** {TradeValues[0]:,d}"
                     },
                     {
                         "name": "Offering",
-                        "value": "\n".join(f"{':warning: ' if rolivalues[str(i['assetId'])]['Projected'] else ''} [{i['name']}](https://www.roblox.com/catalog/{i['assetId']}) ({rolivalues[str(i['assetId'])]['Value']:,d})" for i in tradejson['offers'][1]['userAssets'])+f"\n**Total Value:** {TradeValues[1]:,d}"
+                        "value": "\n".join(f"{':warning: ' if rolivalues[str(i['assetId'])]['Projected'] else ''} [{i['name']}](https://www.roblox.com/catalog/{i['assetId']}) ({rolivalues[str(i['assetId'])]['Value']:,d})" for i in tradejson['offers'][1]['userAssets'])+f"\n**Robux**: {tradejson['offers'][1]['robux']}\n**Total Value:** {TradeValues[1]:,d}"
                     },
                     {
                         "name": "Evaluation",
@@ -47,7 +49,7 @@ def sendnotif(tradejson):
                 "thumbnail": {
                     "url": f"https://www.roblox.com/headshot-thumbnail/image?userId={tradejson['user']['id']}&width=100&height=100&format=png"
                 }, #Makes the image URL the avatar of the user who sent me a trade
-                "color": 0x00ff00 if (TradeValues[1]-TradeValues[0]>0) else 0xff0000, #Sets the colour to a lovely green
+                "color": 0x00ff00 if (TradeValues[1]-TradeValues[0])>0 else 0xff0000, #Sets the colour to a lovely green
                 "footer": #Text at the very bottom
                 { 
                     "icon_url": "https://cdn.discordapp.com/icons/548263085614301186/a_bbb4230eb4af483dee66e996fdd67f18.png?size=1024",
@@ -56,6 +58,7 @@ def sendnotif(tradejson):
             }
         ]
     }
+    url = WinWebhook if (TradeValues[1]-TradeValues[0])>0 else LoseWebhook
     #This ends up looking like this https://gyazo.com/9e27e8579b3e9a8f33bc71ea16e8b64c
     requests.post(url,json=data) #Posts the webhook
 
