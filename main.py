d135ba964d6748fd55c82f655d3c0d289ac79f1c
Erosful <101930449+Erosful@users.noreply.github.com>
2022-07-25 16:01:40 +0100

    Update main.py
    I'll rewrite this in a proper manner soon I promise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json,time,datetime
-from this import d #Json, time, and datetime are all inbuilt libraries with python, so you don't need to install them.
+import json,time,datetime#Json, time, and datetime are all inbuilt libraries with python, so you don't need to install them.
 try: #Requests on the other hand, needs to be installed. This just checks to see if you have it installed..
     import requests
 except: #and if you don't, it'll install it for you!
@@ -12,10 +11,16 @@ with open("config.json","r") as f:
 cookie = data['Cookie']
 WinWebhook = data['WinWebhook']
 LoseWebhook = data['LoseWebhook']
-DiscordID = data['Discord ID']
 def sendnotif(tradejson):
+    with open("config.json","r") as f:
+        data = json.load(f)
     with open("RolimonsItems.json","r") as f: #I open this every time I start the subroutine because I want the most up-to-date values
         rolivalues = json.load(f)
+    DiscordID = data['Discord ID'] #I call these from within the subroutine incase you change them midway through the code.
+    RequestEmoji = data['RequestEmoji']
+    OfferEmoji = data['OfferEmoji']
+    WinEmoji = data['WinEmoji']
+    LoseEmoji = data['LoseEmoji']
     TradeValues = [0,0]
     for a,v in enumerate(tradejson['offers']): #This bit iterates through the offers, and calculates the final values
         for i in v['userAssets']:
@@ -26,11 +31,14 @@ def sendnotif(tradejson):
     if OurRobux != 0:
         OurRobux = f"\nRobux: {tradejson['offers'][0]['robux']} ({(tradejson['offers'][0]['robux']//10)*7})"
     else:
-        OurRobux = ""
+        OurRobux = "\nRobux: 0"
     if TheirRobux != 0:
         TheirRobux = f"\nRobux: {tradejson['offers'][1]['robux']} ({(tradejson['offers'][1]['robux']//10)*7})"
     else:
-        TheirRobux = ""
+        TheirRobux = "\nRobux: 0"
+    GainOrLoss = [WinEmoji,"Gain"] if (TradeValues[1]-TradeValues[0])>0 else [LoseEmoji,"Loss"]
+    ItemGainOrLoss = [WinEmoji,"Gain"] if len(tradejson['offers'][1]['userAssets'])-len(tradejson['offers'][0]['userAssets']) else [LoseEmoji,"Loss"]
+
     #With this, be careful what you edit (I advise you look at https://leovoel.github.io/embed-visualizer/ for more info)
     data = {
         "username" : "Trade Notifier", #You don't NECESSARILY need to have this and the avatar_url field, I just felt like including it [You could change the name on the webhook every time]
@@ -43,16 +51,16 @@ def sendnotif(tradejson):
                 "description": f"This trade was sent by [{tradejson['user']['name']}](https://www.roblox.com/users/{tradejson['user']['id']}/profile)",
                 "fields": [
                     {
-                        "name": "Requesting", #This might be a bit confusing, sorry! Essentially, I add a :warning: if the item is projected, then I post the item name and a link to that item - "\n".join() is just an easier way of sending them all in separate lines
-                        "value": "\n".join(f"{':warning: ' if rolivalues[str(i['assetId'])]['Projected'] else ''}[{i['name']}](https://www.roblox.com/catalog/{i['assetId']}) ({rolivalues[str(i['assetId'])]['Value']:,d})" for i in tradejson['offers'][0]['userAssets'])+f"{OurRobux}\n\n**Total Value:** {TradeValues[0]:,d}"
-                    },
-                    {
-                        "name": "Offering",
+                        "name": f"Offering {OfferEmoji}",
                         "value": "\n".join(f"{':warning: ' if rolivalues[str(i['assetId'])]['Projected'] else ''}[{i['name']}](https://www.roblox.com/catalog/{i['assetId']}) ({rolivalues[str(i['assetId'])]['Value']:,d})" for i in tradejson['offers'][1]['userAssets'])+f"{TheirRobux}\n\n**Total Value:** {TradeValues[1]:,d}"
                     },
                     {
-                        "name": "Evaluation",
-                        "value": f"Value {'Gain' if (TradeValues[1]-TradeValues[0])>0 else 'Loss'}: {abs(TradeValues[1]-TradeValues[0]):,d} ({'+' if (TradeValues[1]-TradeValues[0])>=0 else '-'}{round(abs(((TradeValues[0]-TradeValues[1])/TradeValues[1])*100),2)}%)\nItem {'Gain' if len(tradejson['offers'][1]['userAssets'])-len(tradejson['offers'][0]['userAssets'])>0 else 'Loss'}: {abs(len(tradejson['offers'][1]['userAssets'])-len(tradejson['offers'][0]['userAssets'])):,d}" 
+                        "name": f"Requesting {RequestEmoji}", #This might be a bit confusing, sorry! Essentially, I add a :warning: if the item is projected, then I post the item name and a link to that item - "\n".join() is just an easier way of sending them all in separate lines
+                        "value": "\n".join(f"{':warning: ' if rolivalues[str(i['assetId'])]['Projected'] else ''}[{i['name']}](https://www.roblox.com/catalog/{i['assetId']}) ({rolivalues[str(i['assetId'])]['Value']:,d})" for i in tradejson['offers'][0]['userAssets'])+f"{OurRobux}\n\n**Total Value:** {TradeValues[0]:,d}"
+                    },
+                    {
+                        "name": "Evaluation :scales:",
+                        "value": f"{GainOrLoss[0]} Value {GainOrLoss[1]}: {abs(TradeValues[1]-TradeValues[0]):,d}\n{GainOrLoss[0]} Percentage {GainOrLoss[1]}: {'+' if (TradeValues[1]-TradeValues[0])>=0 else '-'}{round(abs(((TradeValues[1]-TradeValues[0])/TradeValues[0])*100),2)}%\n{ItemGainOrLoss[0]} Item {ItemGainOrLoss[1]}: {abs(len(tradejson['offers'][1]['userAssets'])-len(tradejson['offers'][0]['userAssets'])):,d}"
                     }
                 ], 
                 "thumbnail": {
@@ -69,7 +77,8 @@ def sendnotif(tradejson):
     }
     url = WinWebhook if (TradeValues[1]-TradeValues[0])>0 else LoseWebhook
     #This ends up looking like this https://gyazo.com/9e27e8579b3e9a8f33bc71ea16e8b64c
-    requests.post(url,json=data) #Posts the webhook
+    x = requests.post(url,json=data) #Posts the webhook
+    print(x.status_code)
 def rolimonitemupdater():
     tempdata = {} #Makes an empty dict, this is where all our itemdetails will be added
     x = requests.get("https://www.rolimons.com/itemapi/itemdetails").json()
